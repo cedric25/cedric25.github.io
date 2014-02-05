f0d8f233a3cc9ac9c2d0003901840998a8ef5cd9
Cedric Nicoloso <cedric.nicoloso@gmail.com>
2014-02-05 23:18:22 +0100

    Notifications service

---
+++ BanqueNetFire2/app/js/structure/services.notifications.js
@@ -0,0 +1,74 @@
+'use strict';
+
+angular.module('banqueNetApp.services.notifications', [])
+
+  /**
+   * Service de gestion des messages de validation et d'erreur
+   * A chaque changement de route, les messages sont vidés
+   * Sauf dans le cas où le booléen 'keepMsg' est fixé à 'true'
+   * Dans ce cas, le message "survivra" après le premier changement de route
+   * Ex: Création d'un user, message d'info et redirection vers la liste : le message doit "survivre" à la redirection
+   */
+  .factory('Notifications', function () {
+
+    // private
+    var greenMessage = '';
+    var redMessage = '';
+
+    var keepMessage = false;
+
+    // public
+    return {
+
+      'hasGreenMessage': function () {
+        return greenMessage !== '';
+      },
+
+      /**
+       * @param msg Message à afficher
+       * @param keepMsg Booléen qui indique si le message doit être conservé après un changement de route
+       */
+      'setGreenMessage': function (msg, keepMsg) {
+        redMessage = '';
+        greenMessage = msg;
+        if (keepMsg) {
+          keepMessage = keepMsg;
+        }
+      },
+
+      'getGreenMessage': function () {
+        return greenMessage;
+      },
+
+      'hasRedMessage': function () {
+        return redMessage !== '';
+      },
+
+      'setRedMessage': function (msg, keepMsg) {
+        greenMessage = '';
+        redMessage = msg;
+        if (keepMsg) {
+          keepMessage = keepMsg;
+        }
+      },
+
+      'getRedMessage': function () {
+        return redMessage;
+      },
+
+      'isKeepMessage': function() {
+        return keepMessage;
+      },
+
+      'setKeepMessage': function(value) {
+        keepMessage = value;
+      },
+
+      'clear': function () {
+        greenMessage = '';
+        redMessage = '';
+        keepMessage = false;
+      }
+    }
+
+  });
