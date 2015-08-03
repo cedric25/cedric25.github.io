07f6c2755af226910310af7319723e4ccee9b647
Cedric Nicoloso <cedric.nicoloso@gmail.com>
2015-08-03 22:40:59 +0200

    Ajout des fonts dans le dist

--- appart/gulp/build.js
+++ appart/gulp/build.js
@@ -68,7 +68,12 @@ gulp.task('html', ['inject', 'partials'], function () {
 // Only applies for fonts from bower dependencies
 // Custom fonts are handled by the "other" task
 gulp.task('fonts', function () {
-  return gulp.src($.mainBowerFiles())
+
+  var fontFiles = [
+    'bower_components/bootstrap-sass-official/assets/fonts/bootstrap/*'
+  ];
+
+  return gulp.src(fontFiles)
     .pipe($.filter('**/*.{eot,svg,ttf,woff,woff2}'))
     .pipe($.flatten())
     .pipe(gulp.dest(path.join(conf.paths.dist, '/fonts/')));
